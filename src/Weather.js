0175ba636381e35850972b9d83bd7440c6b80086
Vikocc <vikki.lazorenko@gmail.com>
2022-11-03 17:31:05 +0200

    Add some css

---
+++ src/Weather.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Weather.css";
+
+export default function () {
+  return (
+    <div className="Weather-app">
+      <form>
+        <input
+          type="search"
+          placeholder="&#128269; Search for a city..."
+        ></input>
+      </form>
+      <div className="city">Kyiv</div>
+      <div className="temperature">19℃</div>
+      <div className="main">Mostly Clear</div>
+      <div className="temp-h-l">
+        <span className="high-temp">H: 24℃</span>
+        <span className="low-temp">L: 18℃</span>
+      </div>
+    </div>
+  );
+}
