acaad8d30f264b87a2b508a77629869ba6e1c2f8
Vikocc <vikki.lazorenko@gmail.com>
2022-11-04 23:04:14 +0200

    deploy

--- src/Weather.js
+++ src/Weather.js
@@ -1,47 +1,23 @@
-import axios from "axios";
-import React, { useState } from "react";
+import React from "react";
 import "./Weather.css";
 
-export default function () {
-  let [weather, setWeather] = useState({ ready: false });
-
-  function showWeather(response) {
-    setWeather({
-      ready: true,
-      name: response.data.city,
-      temperature: Math.round(response.data.daily[0].temperature.day),
-      minTemp: Math.round(response.data.daily[0].temperature.minimum),
-      maxTemp: Math.round(response.data.daily[0].temperature.maximum),
-      main: response.data.daily[0].condition.description,
-    });
-  }
-
-  if (weather.ready) {
-    return (
-      <div className="Weather-app">
-        <form>
-          <input
-            type="search"
-            placeholder="&#128269; Search for a city..."
-          ></input>
-        </form>
-        <div className="city">{weather.name}</div>
-        <div className="temperature">{weather.temperature}</div>
-        <div className="main">{weather.main}</div>
-        <div className="temp-h-l">
-          <span className="max-temp">H: {weather.maxTemp}℃</span>
-          <span className="min-temp">L: {weather.minTemp}℃</span>
-        </div>
-
-        <div className="forecast">04 nov</div>
+export default function Weather() {
+  return (
+    <div className="Weather-app">
+      <form>
+        <input
+          type="search"
+          placeholder="&#128269; Search for a city..."
+        ></input>
+      </form>
+      <div className="city">Kyiv</div>
+      <div className="temperature">19℃</div>
+      <div className="main">Mostly Clear</div>
+      <div className="temp-h-l">
+        <span className="high-temp">H: 24℃</span>
+        <span className="low-temp">L: 18℃</span>
       </div>
-    );
-  } else {
-    let key = "37do4ft2cfe4091641be09b5a6ea838c";
-    let city = "kyiv";
-
-    let url = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${key}`;
-    axios.get(url).then(showWeather);
-    return "Loading...";
-  }
+      <div className="forecast">04 nov</div>
+    </div>
+  );
 }
